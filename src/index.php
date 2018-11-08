64add2a571ff08c28b95ec133dced4ee60661fda
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2018-11-08 14:05:48 +0100

    UPDATE: Layouting and disigning the home page

--- src/index.php
+++ src/index.php
@@ -7,6 +7,7 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!--Import materialize.css-->
         <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+        <link rel="stylesheet" href="css/styles.css">
 
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -16,26 +17,27 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
+
     </head>
     <body>
         <header>
-            <nav class="top-nav">
-                <div class="container">
-                    <div class="nav-wrapper">
-                        <div class="row">
-                            <div class="col s12 m10 offset-m1">
-                                <h5 class="header">Conduite de Projet</h5>
+            <div class="navbar-fixed">
+                <nav class="top-nav">
+                    <div class="container">
+                        <div class="nav-wrapper">
+                            <div class="row">
+                                <div class="col s12 m10 offset-m1">
+                                    <h5 class="header">Conduite de Projet</h5>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </nav>
-            <div class="container">
-                <a href="#" data-target="nav-mobile" class="top-nav sidenav-trigger full hide-on-large-only">
-                    <i class="material-icons">menu</i>
-                </a>
+                </nav>
             </div>
-            <ul id="nav-mobile" class="sidenav sidenav-fixed" style="transform: translateX(0%);">
+            <div class="container">
+                <a href="#" data-target="mobile-demo" class="top-nav sidenav-trigger full hide-on-large-only"><i class="material-icons">menu</i></a>
+            </div>
+            <ul class="sidenav sidenav-fixed" id="mobile-demo" style="overflow: auto; transform: translateX(0%);">
                 <li class="logo">
                     <a id="logo-container" href="./" class="brand-logo"></a>
                 </li>
@@ -46,7 +48,6 @@
                     <a href="./userStory/addUserStory.php"><i class="material-icons left">playlist_add</i>Ajouter user story</a>
                 </li>
             </ul>
-
         </header>
         <main>
             <div class="row">
@@ -76,6 +77,12 @@
         </main>
 
         <!--JavaScript at end of body for optimized loading-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script type="text/javascript">
+        $(document).ready(function(){
+            $('.sidenav').sidenav();
+          });
+        </script>
     </body>
 </html>
