69ecc483b0c9826f9ecbb26fbdce1ed29b69625c
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2018-11-08 18:50:59 +0100

    Adding style to addUserStory.php Add header.html to include in files when we need it

--- src/userStory/addUserStory.php
+++ src/userStory/addUserStory.php
@@ -49,29 +49,73 @@ function isIdUnique($id, $db)
     <meta charset="utf-8" />
     <title>Ajout de user story</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../css/materialize.css" media="screen,projection" />
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
+    <script type="text/javascript" src="/js/materialize.min.js" defer></script>
 </head>
 
 <body>
-    <form method="post" action="addUserStory.php">
-        <p>
-            Veuillez entrer les informations de votre user story</br>
-            (les champs précédé d'un * sont obligatoire):</br>
-        </p>
-        <div><?php echo $idNotUnique; ?></div>
-        Id: * <input type="number" name="idUserStory" min=0 placeholder="Id unique"
-            required /></br>
-        Description: * <textarea name="descUserStory" cols="40" rows="5"
-            required></textarea></br>
-        Difficulté: * <select name="diffUserStory">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="8">8</option>
-            <option value="13">13</option>
-            </select></br>
-        Priorité: <input type="number" name="prioUserStory" min=1 /></br>
-        <input type="submit" value="Valider" /><a href="index.php">annuler</a>
-    </form>
+    <?php include("../header.html")?>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m8 offset-m2">
+                    <div id="grid-container" class="section scrollspy">
+                        <form class="col s12" method="post" action="addUserStory.php">
+                            <h5 style="text-align: center;">Créer une nouvelle User Story </h5>
+                            <div class="row">
+                                <p>
+                                    Veuillez entrer les informations de votre user story</br>
+                                    (les champs précédé d'un * sont obligatoire):</br>
+                                </p>
+                            </div>
+                            <div class="row">
+                                <?php echo $idNotUnique; ?>
+                                <div class="input-field col s12">
+                                    <label for="idUserStory">Id *</label>
+                                    <input class="validate" type="number" name="idUserStory" min=0 required />
+                                    <span class="helper-text" data-error="Entrez un nombre" data-success="right">Id unique</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <label for="descUserStory">Description *</label>
+                                    <textarea class="materialize-textarea" name="descUserStory" maxlength="500" data-length="500" required></textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s6">
+                                    <label for="diffUserStory">Difficulté *</label>
+                                    <input class="validate" type="number" name="diffUserStory" min=1 required />
+                                    <span class="helper-text" data-error="Entrez un nombre" data-success="right"></span>
+                                </div>
+                                <div class="input-field col s6">
+                                    <label for="prioUserStory">Priorité *</label>
+                                    <input class="validate" type="number" name="prioUserStory" min=1 max=3 />
+                                    <span class="helper-text" data-error="Entrez un nombre entre 1 et 3" data-success="right">1=Haut,
+                                        2=Moyen et 3=Bas</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col s6">
+                                    <button type="submit" name="newUserStory" class="btn waves-effect waves-light">
+                                        Créer
+                                        <i class="material-icons left">check_circle</i>
+                                    </button>
+                                </div>
+                                <div class="col s6">
+                                    <button type="button" name="cancel" class="btn waves-effect waves-light" onclick="window.history.back()">
+                                        Annuler
+                                        <i class="material-icons left">cancel</i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 </body>
+
 </html>
