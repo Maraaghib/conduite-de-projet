1d47e7bf288594b4904d9508475ae01cd1c343c5
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2018-11-21 21:17:26 +0100

    Remove materialize files and use of a CDN instead

--- src/sprint/newSprint.php
+++ src/sprint/newSprint.php
@@ -90,7 +90,7 @@ function isValidDate($date, $project) {
     <link type="text/css" rel="stylesheet" href="../css/materialize.css" media="screen,projection" />
     <link rel="stylesheet" href="/css/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
-    <script type="text/javascript" src="/js/materialize.min.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" media="screen,projection"/>
 </head>
 
 <body>
@@ -155,7 +155,7 @@ function isValidDate($date, $project) {
 
     <!--JavaScript at end of body for optimized loading-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script type="text/javascript" src="/js/scripts.js"></script>
 </body>
 
--- src/userStory/updateUserStory.php
+++ src/userStory/updateUserStory.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET["projectName"]) && isset
     <link type="text/css" rel="stylesheet" href="../css/materialize.css" media="screen,projection" />
     <link rel="stylesheet" href="/css/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
-    <script type="text/javascript" src="/js/materialize.min.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" media="screen,projection"/>
 </head>
 
 <body>
