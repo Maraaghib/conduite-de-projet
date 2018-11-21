478b4366d56a07b23e9c4861f7ceef2a39537eb3
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2018-11-21 23:36:26 +0100

    Adding "Edit" button to each US and reorganizing the table

--- src/userStory/listBacklog.php
+++ src/userStory/listBacklog.php
@@ -3,35 +3,40 @@
         <tr>
             <th>Id</th>
             <th>Description</th>
-            <th>Priorité</th>
             <th>Difficulté</th>
-            <th>Action</th>
+            <th>Priorité</th>
+            <th>Sprint</th>
+            <th>Modifier</th>
+            <th>Supprimer</th>
         </tr>
     </thead>
     <tbody>
         <?php
-        foreach ($backlog as list($pn, $id, $desc, $prio, $diff)) {
-            echo "<tr> <td id='id$id'>#$id</td> <td>$desc</td> <td>$prio</td> <td>$diff</td> <td>";
+        foreach ($backlog as list($pn, $id, $desc, $diff, $prio, $sprint)) {
+            echo "<tr> <td id='id$id'>#$id</td> <td>$desc</td> <td>$diff</td> <td>$prio</td> <td>$sprint</td>";
         ?>
-        <!-- Modal Trigger -->
-        <a href="#delete-modal-<?php echo $id ?>" class="btn-floating waves-effect waves-light modal-trigger red"><i class="material-icons">delete</i></a>
-        <!-- Modal Structure -->
-        <div id="delete-modal-<?php echo $id ?>" class="modal">
-            <div class="modal-content">
-                <h4>Suppression</h4>
-                <p>Êtes-vous sûr de vouloir supprimer le <strong>User Story #<?php echo $id; ?> ?</strong> </p>
+            <td>
+                <a href="/userStory/updateUserStory.php?projectName=<?php echo $project['projectName']; ?>&idUserStory=<?php echo $id; ?>" class="btn-floating waves-effect waves-light modal-trigger yellow"><i class="material-icons">edit</i></a>
+            </td>
+            <td>
+            <!-- Modal Trigger -->
+            <a href="#delete-modal-<?php echo $id ?>" class="btn-floating waves-effect waves-light modal-trigger red"><i class="material-icons">delete</i></a>
+            <!-- Modal Structure -->
+            <div id="delete-modal-<?php echo $id ?>" class="modal">
+                <div class="modal-content">
+                    <h4>Suppression</h4>
+                    <p>Êtes-vous sûr de vouloir supprimer le <strong>User Story #<?php echo $id; ?> ?</strong> </p>
+                </div>
+                <div class="modal-footer">
+                    <form action="/userStory/removeUserStory.php" method="post">
+                        <input type="hidden" name="projectName" value="<?php echo htmlspecialchars($pn)?>">
+                        <input type="hidden" name="idUserStory" value=<?php echo $id?>>
+                        <button type="submit" class="modal-close waves-effect waves-red btn-flat">Supprimer<i class="material-icons left">check_circle</i></button>
+                        <button type="button" class="modal-close waves-effect waves-green btn-flat">Annuler<i class="material-icons left">cancel</i></button>
+                    </form>
+                </div>
             </div>
-            <div class="modal-footer">
-                <form action="/userStory/removeUserStory.php" method="post">
-                    <input type="hidden" name="projectName" value="<?php echo htmlspecialchars($pn)?>">
-                    <input type="hidden" name="idUserStory" value=<?php echo $id?>>
-                    <button type="submit" class="modal-close waves-effect waves-red btn-flat">Supprimer<i class="material-icons left">check_circle</i></button>
-                    <button type="button" class="modal-close waves-effect waves-green btn-flat">Annuler<i class="material-icons left">cancel</i></button>
-                </form>
-            </div>
-        </div>
         <?php
-        echo "</td> </tr>";
         }
         ?>
     </tbody>
