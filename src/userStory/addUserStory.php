79fe8709edc2845ad81e4aee321e2b95e0e435d3
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2018-11-22 00:53:46 +0100

    Fix bug in loading materialize.js

--- src/userStory/addUserStory.php
+++ src/userStory/addUserStory.php
@@ -140,7 +140,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET["projectName"]) && testP
 
     <!--JavaScript at end of body for optimized loading-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" defer></script>
     <script type="text/javascript" src="/js/scripts.js"></script>
 </body>
 
