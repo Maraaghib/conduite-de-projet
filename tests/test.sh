4af5468fe4247544967cd64a9222ab947b29f597
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2018-11-29 19:19:54 +0100

    Fix test.sh always returning true (again)

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,11 @@
 python3 -m coverage erase
 if !(python3 -m coverage run testNewProject.py); then
-    exit
+    exit 1
 fi
 if !(python3 -m coverage run -a testAddUserStory.py); then
-    exit
+    exit 1
 fi
-if !(python3 -m coverage run -a testRemoveUserStory.py);
-    exit
+if !(python3 -m coverage run -a testRemoveUserStory.py); then
+    exit 1
 fi
 python3 -m coverage xml -i
