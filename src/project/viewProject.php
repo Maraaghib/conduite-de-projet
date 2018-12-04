d43d41cf1e595e295d2be7f5a0261ac38f053173
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2018-12-04 05:16:21 +0100

    Implementing on client side of the listing and adding collaborators

--- src/project/viewProject.php
+++ src/project/viewProject.php
@@ -200,15 +200,62 @@
                                 </div>
                             </div>
                             <div id="tab-swipe-5" class="col s12 transp-cyan">
-                                <div class="row" style="margin: 10px;">
-                                    <div class="s12">
-                                        <h4>Collaborateurs</h4>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt umtest labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt umtest labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                        </p>
+                                <div class="container">
+                                    <div class="row" style="margin: 10px;">
+                                        <div class="section">
+                                            <div class="s12">
+                                                <h4>Collaborateurs</h4>
+                                                <div class="row">
+                                                    <div class="col s12 chip">
+                                                        <img class="collab-img" src="/img/avatar.png" alt="Propriétaire du projet" title="Propriétaire du projet">
+                                                        <strong class="collab-name">Hamza SEYE</strong>
+                                                        Ajouté(e) le: 04-12-2018
+                                                        <i data-target="remove-collab-modal" class="modal-trigger material-icons" title="Retirer">close</i>
+                                                        <!-- Modal Structure -->
+                                                        <div id="remove-collab-modal" class="modal">
+                                                            <div class="modal-content">
+                                                                <h4>Suppression</h4>
+                                                                <p>Êtes-vous sûr de vouloir retirer <strong>{Collab-name}</strong> du projet <strong>{Project-name}</strong> ?</p>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <form action="" method="post">
+                                                                    <input type="hidden" name="projectID" value="<?php //echo $project['projectID']; ?>">
+                                                                    <input type="hidden" name="userEmail" value=<?php //echo $id?>>
+                                                                    <button name="confirmRemoveCollab" type="submit" class="modal-close waves-effect waves-red btn-flat">Retirer<i class="material-icons left" aria-hidden="true">check_circle</i></button>
+                                                                    <button type="button" class="modal-close waves-effect waves-green btn-flat">Annuler<i class="material-icons left" aria-hidden="true">cancel</i></button>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="divider"></div>
+                                        <div class="section">
+                                            <div class="col s12">
+                                                <div class="row">
+                                                    <div class="input-field col s8" style="display: table-cell; padding: 0px; margin-top: 0px; margin-bottom: 30px;">
+                                                        <i class="material-icons prefix">group_add</i>
+                                                        <input type="text" id="autocomplete-input" class="autocomplete" name="" value="">
+                                                        <label for="autocomplete-input">E-mail</label>
+                                                    </div>
+                                                    <span style="display: table-cell;">
+                                                        <button class="btn waves-effect waves-light" type="button" name="button">Ajouter collaborateur</button>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <script type="text/javascript">
+                                            document.addEventListener('DOMContentLoaded', function() {
+                                                var elem = document.querySelector('.autocomplete');
+                                                var instance = M.Autocomplete.init(elem);
+                                                instance.updateData({
+                                                    "Serigne": '/img/avatar.png',
+                                                    "Amsatou": '/img/avatar.png',
+                                                    "Hamza": '/img/avatar.png'
+                                                });
+                                            });
+                                        </script>
                                     </div>
                                 </div>
                             </div>
